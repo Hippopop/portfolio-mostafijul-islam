5f919920e1425e1505df5d837af18403d91e69e7
Mostafijul Islam <mostafijul1000@gmail.com>
2024-01-30 18:00:19 +0600

    chore: testing publish system.

---
+++ lib/src/pages/homepage/sections/top_navigation_bar.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio_mostafij/src/services/theme/app_theme.dart';
+import 'package:portfolio_mostafij/src/utilities/responsive/responsive_parent.dart';
+
+import '../widgets/header_section_button.dart';
+
+class TopNavigationBarSection extends StatelessWidget {
+  const TopNavigationBarSection({
+    super.key,
+  });
+
+  final List<String> sections = const [
+    "Home",
+    "About",
+    "Tools",
+    "Projects",
+    "Blog",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: context.responsiveStateListener,
+      builder: (context, responsiveState, child) {
+        return ColoredBox(
+          color: context.color.mainBackground.withOpacity(0.3),
+          child: SafeArea(
+            child: SizedBox(
+              height: 60,
+              width: double.infinity,
+              child: MaterialTwoSpecificationWrapper(
+                state: context.responsiveState,
+                child: Row(
+                  children: [
+                    FittedBox(
+                      child: Text(
+                        "Mostafij",
+                        softWrap: true,
+                        overflow: TextOverflow.ellipsis,
+                        style: context.text.headlineSmall?.merge(
+                          GoogleFonts.righteous(color: context.color.opposite),
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    if (context.responsiveState > ResponsiveState.sm)
+                      LimitedBox(
+                        maxWidth: 420,
+                        child: Row(
+                          children: sections
+                              .map(
+                                (e) => Expanded(
+                                  child: SectionButton(
+                                    text: e,
+                                    onTap: () {},
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/src/pages/homepage/widgets/header_section_button.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio_mostafij/src/services/theme/app_theme.dart';
+
+class SectionButton extends StatefulWidget {
+  const SectionButton({
+    super.key,
+    required this.text,
+    required this.onTap,
+  });
+
+  final String text;
+  final VoidCallback onTap;
+
+  @override
+  State<SectionButton> createState() => _SectionButtonState();
+}
+
+class _SectionButtonState extends State<SectionButton> {
+  bool _isHovered = false;
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onHover: (value) {
+        setState(() {
+          _isHovered = value;
+        });
+      },
+      style: TextButton.styleFrom(
+        textStyle: GoogleFonts.raleway(),
+      ),
+      onPressed: widget.onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: IntrinsicWidth(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 3),
+                  child: Text(widget.text),
+                ),
+              ),
+              AnimatedSize(
+                duration: Durations.long1,
+                child: SizedBox(
+                  height: _isHovered ? 3 : 0,
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: context.color.opposite,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: AnimatedSize(
+                    duration: Durations.long1,
+                    child: SizedBox(
+                      height: _isHovered ? 1 : 0,
+                      width: _isHovered ? double.infinity : 0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
