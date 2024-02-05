887f298615eb2499dd98b69f5f688ec980950273
Mostafijul Islam <mostafijul1000@gmail.com>
2024-02-05 10:01:40 +0600

    fix: made rive face responsive!

--- lib/src/pages/homepage/sections/hero_section.dart
+++ lib/src/pages/homepage/sections/hero_section.dart
@@ -18,128 +18,170 @@ class HeroSection extends StatelessWidget {
       builder: (context, state, child) {
         return MaterialTwoSpecificationWrapper(
           state: context.responsiveState,
-          child: IntrinsicHeight(
-            child: Row(
-              children: [
-                Flexible(
-                  child: ConstrainedBox(
-                    constraints: BoxConstraints(
-                      maxWidth: ResponsiveState.ts.max,
-                      minWidth: ResponsiveState.ts.min,
+          child: switch (state) {
+            ResponsiveState.sm ||
+            ResponsiveState.ts ||
+            ResponsiveState.xs =>
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxHeight: 340,
+                      maxWidth: 340,
+                      minHeight: 250,
+                      minWidth: 250,
                     ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        72.height,
-                        RichText(
-                          softWrap: true,
-                          text: TextSpan(
-                            text: "Hello! 👋\nI'm ",
-                            style: context.text.headlineMedium?.merge(
-                              GoogleFonts.sofia(
-                                color: context.color.opposite,
-                              ),
-                            ),
-                            children: [
-                              TextSpan(
-                                text: "Mostafijul Islam",
-                                style: context.text.headlineMedium?.merge(
-                                  GoogleFonts.sourceSans3(
-                                    fontWeight: FontWeight.bold,
-                                    decorationColor: Colors.blue,
-                                    color: context.color.opposite,
-                                    decoration: TextDecoration.underline,
-                                    decorationStyle: TextDecorationStyle.wavy,
-                                  ),
-                                ),
-                              ),
-                              const TextSpan(
-                                text: ".\n",
-                              ),
-                              TextSpan(
-                                text: "I'm a ",
-                                style: context.text.headlineMedium?.merge(
-                                  GoogleFonts.sofia(
-                                    color: context.color.opposite,
-                                  ),
-                                ),
-                              ),
-                              WidgetSpan(
-                                child: AnimatedTextKit(
-                                  repeatForever: true,
-                                  animatedTexts: [
-                                    ColorizeAnimatedText(
-                                      "Flutter Developer",
-                                      speed: Durations.extralong4,
-                                      textStyle:
-                                          context.text.headlineMedium!.merge(
-                                        GoogleFonts.sofia(
-                                          fontWeight: FontWeight.bold,
-                                          decorationColor: Colors.blue,
-                                          color: context.color.opposite,
-                                          decorationStyle:
-                                              TextDecorationStyle.wavy,
-                                        ),
-                                      ),
-                                      colors: [
-                                        Colors.blue,
-                                        Colors.grey,
-                                        Colors.blue,
-                                        Colors.grey,
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              const TextSpan(
-                                text: ".",
-                              ),
-                            ],
-                          ),
-                        ),
-                        18.height,
-                        Expanded(
-                          child: Text(
-                            "I build cross-platform apps using Flutter. But just like Flutter I specialize at building Mobile apps. Basically Android & iOS.",
-                            softWrap: true,
-                            maxLines: 100,
-                            overflow: TextOverflow.ellipsis,
-                            style: GoogleFonts.poppins(
-                              fontWeight: FontWeight.w300,
-                              color: context.color.secondaryText,
-                            ),
-                          ),
-                        ),
-                      ],
+                    child: const FittedBox(
+                      child: HeroRiveFace(),
+                    ),
+                  ),
+                  const HeroDetailsSection(),
+                ],
+              ),
+            _ => IntrinsicHeight(
+                child: Row(
+                  children: [
+                    const Flexible(
+                      child: HeroDetailsSection(),
+                    ),
+                    4.width,
+                    const Expanded(
+                      child: SizedBox.expand(
+                        child: HeroRiveFace(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          },
+        );
+      },
+    );
+  }
+}
+
+class HeroRiveFace extends StatelessWidget {
+  const HeroRiveFace({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.square(
+      dimension: 500,
+      child: Center(
+        child: RiveAnimation.asset(
+          "assets/animations/rive/face.riv",
+          onInit: (artBoard) {
+            final controller = StateMachineController.fromArtboard(
+              artBoard,
+              "State Machine 1",
+            );
+            if (controller != null) {
+              artBoard.addController(controller);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class HeroDetailsSection extends StatelessWidget {
+  const HeroDetailsSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxWidth: ResponsiveState.ts.max,
+        minWidth: ResponsiveState.ts.min,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          72.height,
+          RichText(
+            softWrap: true,
+            text: TextSpan(
+              text: "Hello! 👋\nI'm ",
+              style: context.text.headlineMedium?.merge(
+                GoogleFonts.sofia(
+                  color: context.color.opposite,
+                ),
+              ),
+              children: [
+                TextSpan(
+                  text: "Mostafijul Islam",
+                  style: context.text.headlineMedium?.merge(
+                    GoogleFonts.sourceSans3(
+                      fontWeight: FontWeight.bold,
+                      decorationColor: Colors.blue,
+                      color: context.color.opposite,
+                      decoration: TextDecoration.underline,
+                      decorationStyle: TextDecorationStyle.wavy,
                     ),
                   ),
                 ),
-                4.width,
-                Expanded(
-                  child: SizedBox.expand(
-                    child: Center(
-                      child: RiveAnimation.asset(
-                        "assets/animations/rive/face.riv",
-                        onInit: (artBoard) {
-                          final controller =
-                              StateMachineController.fromArtboard(
-                            artBoard,
-                            "State Machine 1",
-                          );
-                          if (controller != null) {
-                            artBoard.addController(controller);
-                          }
-                        },
-                      ),
+                const TextSpan(
+                  text: ".\n",
+                ),
+                TextSpan(
+                  text: "I'm a ",
+                  style: context.text.headlineMedium?.merge(
+                    GoogleFonts.sofia(
+                      color: context.color.opposite,
                     ),
                   ),
+                ),
+                WidgetSpan(
+                  child: AnimatedTextKit(
+                    repeatForever: true,
+                    animatedTexts: [
+                      ColorizeAnimatedText(
+                        "Flutter Developer",
+                        speed: Durations.extralong4,
+                        textStyle: context.text.headlineMedium!.merge(
+                          GoogleFonts.sofia(
+                            fontWeight: FontWeight.bold,
+                            decorationColor: Colors.blue,
+                            color: context.color.opposite,
+                            decorationStyle: TextDecorationStyle.wavy,
+                          ),
+                        ),
+                        colors: [
+                          Colors.blue,
+                          Colors.grey,
+                          Colors.blue,
+                          Colors.grey,
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const TextSpan(
+                  text: ".",
                 ),
               ],
             ),
           ),
-        );
-      },
+          18.height,
+          Text(
+            "I build cross-platform apps using Flutter. But just like Flutter I specialize at building Mobile apps. Basically Android & iOS.",
+            softWrap: true,
+            maxLines: 100,
+            overflow: TextOverflow.ellipsis,
+            style: GoogleFonts.poppins(
+              fontWeight: FontWeight.w300,
+              color: context.color.secondaryText,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
