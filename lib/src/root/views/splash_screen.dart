d136c5cec608f426544be1d595fc3c90d3063ad7
Mostafijul Islam <mostafijul1000@gmail.com>
2024-02-10 21:28:28 +0600

    feat: simple auto scroll added!

--- lib/src/root/views/splash_screen.dart
+++ lib/src/root/views/splash_screen.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:portfolio_mostafij/src/services/theme/app_theme.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends ConsumerWidget {
   const SplashScreen({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: Text(
-              "Splash Screen",
-            ),
-          ),
-        ),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      theme: lightTheme,
+      home: const Scaffold(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
